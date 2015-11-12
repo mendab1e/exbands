4ea3cfe095de2f7ea8905b2effede5468b5c301a
mendable <yanberdint@gmail.com>
2015-11-12 18:49:11 +0300

    parallel bands queries

--- lib/exbands.ex
+++ lib/exbands.ex
@@ -1,7 +1,26 @@
 defmodule Exbands do
-  @limit 2
+  @limit 1
 
-  def start do
-    Exbands.Lastfm.get_bands("a", @limit)
+  def main do
+    {:ok, pg_pid} = Exbands.Postgres.connect
+
+    bands = Enum.to_list(?a..?z) ++ Enum.to_list(?а..?я)
+    |> load_bands
+
+    bands |> Enum.count |> IO.puts
+    bands |> Enum.join(", ") |> IO.puts
+  end
+
+  defp load_bands(letters) do
+    letters
+    |> Enum.map(&(spawn(Exbands, :bands_by_letter, [self, <<&1 :: utf8>>])))
+    |> Enum.map(fn pid ->
+         receive do {^pid, bands} -> bands end
+       end)
+    |> List.flatten
+  end
+
+  def bands_by_letter(caller, letter) do
+    send caller, {self, Exbands.Lastfm.get_bands(letter, @limit)}
   end
 end
