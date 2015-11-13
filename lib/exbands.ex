879933c0471b1de8ca9d3bf7179ce9a071b8c1ea
mendable <yanberdint@gmail.com>
2015-11-13 18:35:17 +0300

    process data and store in pg

--- lib/exbands.ex
+++ lib/exbands.ex
@@ -1,26 +1,34 @@
 defmodule Exbands do
-  @limit 1
+  alias Exbands.Postgres, as: Postgres
+  alias Exbands.Lastfm, as: Lastfm
+  @limit 1000
 
   def main do
-    {:ok, pg_pid} = Exbands.Postgres.connect
+    {:ok, pg_pid} = Postgres.connect
+    bands_in_db = Postgres.load_bands(pg_pid)
 
     bands = Enum.to_list(?a..?z) ++ Enum.to_list(?а..?я)
-    |> load_bands
+    |> load_bands_from_lastfm
+    |> (&(&1 -- bands_in_db)).()
+    |> Postgres.store_bands(pg_pid)
 
-    bands |> Enum.count |> IO.puts
-    bands |> Enum.join(", ") |> IO.puts
+    # bands |> Enum.count |> IO.puts
+    # bands |> Enum.join(", ") |> IO.puts
   end
 
-  defp load_bands(letters) do
+  defp load_bands_from_lastfm(letters) do
     letters
     |> Enum.map(&(spawn(Exbands, :bands_by_letter, [self, <<&1 :: utf8>>])))
     |> Enum.map(fn pid ->
          receive do {^pid, bands} -> bands end
        end)
     |> List.flatten
+    |> Enum.uniq
   end
 
   def bands_by_letter(caller, letter) do
-    send caller, {self, Exbands.Lastfm.get_bands(letter, @limit)}
+    bands = Lastfm.get_bands(letter, @limit)
+    IO.puts "#{@limit} bands by letter #{letter} have been loaded from lastfm"
+    send caller, {self, bands}
   end
 end
--- lib/exbands/postgres.ex
+++ lib/exbands/postgres.ex
@@ -2,6 +2,7 @@ defmodule Exbands.Postgres do
   @username Application.get_env(:exbands, :username)
   @password Application.get_env(:exbands, :password)
   @database Application.get_env(:exbands, :database)
+  @batch_size 1000
 
   def connect do
     Postgrex.Connection.start_link(
@@ -12,10 +13,21 @@ defmodule Exbands.Postgres do
     )
   end
 
+  def store_bands([], _) do
+    IO.puts "All bands have been written to db"
+  end
+
   def store_bands(bands, pid) do
+    {batch, remain_bands} = bands |> Enum.split @batch_size
+    write_bands_to_db(batch, pid)
+    store_bands(remain_bands, pid)
+  end
+
+  defp write_bands_to_db(bands, pid) do
     bands_query = build_bands_query(bands)
-    pid |> Postgrex.Connection.query!("INSERT INTO artists \
+    Postgrex.Connection.query!(pid, "INSERT INTO artists \
       (name, created_at, updated_at) VALUES #{bands_query}", [])
+    IO.puts "#{@batch_size} bands have been written"
   end
 
   def load_bands(pid) do
@@ -28,12 +40,13 @@ defmodule Exbands.Postgres do
   defp build_bands_query(bands) do
     datetime = formated_datetime
     bands
+    |> Enum.map(&(String.replace(&1, "'", "''")))
     |> Enum.map(&("('#{&1}', '#{datetime}', '#{datetime}')"))
     |> Enum.join(", ")
   end
 
   defp formated_datetime do
     {{y, m, d}, {h, min, s}} = :calendar.local_time()
-    "#{y}-#{m}-#{d} #{h}:#{m}:#{s}"
+    "#{y}-#{m}-#{d} #{h}:#{min}:#{s}"
   end
 end
